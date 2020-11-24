3087b444cec70986dc5c95521aff53b21db2bc10
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-24 21:56:01 +0530

    Added: Test code to add multiple employee to payroll database

---
+++ src/test/java/com/blz/employee_multithreading/EmployeePayrollTest.java
@@ -0,0 +1,42 @@
+package com.blz.employee_multithreading;
+
+import java.sql.SQLException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.blz.employee_multithreading.EmployeePayrollService.IOService;
+
+public class EmployeePayrollTest {
+
+	private static EmployeePayrollService employeePayrollService;
+
+	@BeforeClass
+	public static void createEmployeePayrollObj() {
+		employeePayrollService = new EmployeePayrollService();
+		System.out.println("Welcome to the Employee Payroll Program.. ");
+	}
+
+	@Test
+	public void given6Employees_WhenAddedDataToDB_ShouldMatchEmployeesEnteries()
+			throws EmployeePayrollException, SQLException {
+		EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(0, "Anil", "M", 1000000, LocalDate.now()),
+				new EmployeePayrollData(0, "Balu", "M", 2000000, LocalDate.now()),
+				new EmployeePayrollData(0, "Anjani", "F", 3000000, LocalDate.now()),
+				new EmployeePayrollData(0, "Sunil", "M", 4000000, LocalDate.now()),
+				new EmployeePayrollData(0, "Pooja", "F", 5000000, LocalDate.now()),
+				new EmployeePayrollData(0, "Bantu", "M", 6000000, LocalDate.now()), };
+
+		employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
+		Instant start = Instant.now();
+		employeePayrollService.addEmployeePayrollData(Arrays.asList(arrayOfEmps));
+		Instant end = Instant.now();
+		System.out.println("Duration without thread: " + Duration.between(start, end));
+		Assert.assertEquals(15, employeePayrollService.countEnteries(IOService.DB_IO));
+	}
+}
